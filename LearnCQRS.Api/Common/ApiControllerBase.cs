7f73508ef474c997df43485aba5a436ecc8b5c72
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2026-05-26 18:54:05 +0545

    Add AppControllerBase to handle error responses in a standardized way

---
+++ LearnCQRS.Api/Common/ApiControllerBase.cs
@@ -0,0 +1,52 @@
+﻿using ErrorOr;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace LearnCQRS.Api.Common;
+
+[ApiController]
+public abstract class AppControllerBase : ControllerBase
+{
+    [NonAction]
+    protected IActionResult Problem(List<Error> errors)
+    {
+        if (errors.Count is 0)
+            return base.Problem();
+
+        return errors.All(e => e.Type == ErrorType.Validation)
+            ? ValidationProblem(errors)
+            : Problem(errors[0]);
+    }
+
+    [NonAction]
+    private IActionResult Problem(Error error)
+    {
+        var statusCode = error.Type switch
+        {
+            ErrorType.Conflict => StatusCodes.Status409Conflict,
+            ErrorType.Validation => StatusCodes.Status400BadRequest,
+            ErrorType.NotFound => StatusCodes.Status404NotFound,
+            ErrorType.Unauthorized => StatusCodes.Status401Unauthorized,
+            ErrorType.Forbidden => StatusCodes.Status403Forbidden,
+            _ => StatusCodes.Status500InternalServerError
+        };
+
+        return base.Problem(
+            statusCode: statusCode,
+            title: error.Description
+        );
+    }
+
+    [NonAction]
+    private IActionResult ValidationProblem(List<Error> errors)
+    {
+        var modelState = new ModelStateDictionary();
+
+        foreach (var error in errors)
+        {
+            modelState.AddModelError(error.Code, error.Description);
+        }
+
+        return ValidationProblem(modelState);
+    }
+}
