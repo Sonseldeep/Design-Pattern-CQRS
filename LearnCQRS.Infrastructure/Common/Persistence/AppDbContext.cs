aa1ba679a7fa4f2b512e47dfac627e139700d6b2
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2026-05-26 18:54:19 +0545

    Add AppDbContext for database context and unit of work implementation

---
+++ LearnCQRS.Infrastructure/Common/Persistence/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using LeanrCQRS.Domain.Students;
+using LearnCQRS.Application.Common.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace LearnCQRS.Infrastructure.Common.Persistence;
+
+public class AppDbContext : DbContext, IUnitOfWork
+{
+    public DbSet<Student> Students { get; set; } = null!;
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        base.OnModelCreating(modelBuilder);
+    }
+    
+
+    public async Task CommitChangesAsync(CancellationToken cancellationToken = default)
+    {
+        await base.SaveChangesAsync(cancellationToken);
+    }
+}
+
